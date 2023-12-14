9cd0ab5f2af7783b9053db845d263b04bc785a2a
Luis Barrueco <luis.barrueco@gmail.com>
2023-12-14 19:01:59 -0300

    Add dummy interval-data implementation

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select, DateTime, Column
+from typing import Optional
 from datetime import datetime
 
 
@@ -8,3 +9,34 @@ class CarbonData(SQLModel, table=True):
         sa_column=Column(DateTime(timezone=True), primary_key=True)
     )
     value: float
+
+
+class Interval(SQLModel, table=False):
+    start: datetime = Field(primary_key=True)
+    end: datetime = Field(primary_key=True)
+    electricity_consumption: int = Field(nullable=False)
+    net_electricity_consumption: Optional[int]
+    electricity_demand: Optional[int]
+    gas_consumption: Optional[float]
+
+
+# These are the interval data points that we're going to be storing in the database.
+# They have all the same elements as the Interval objects defined above, but also
+# include the ID of a customer so we can tell them apart.
+class CustomerInterval(Interval, table=True):
+    customer: int = Field(primary_key=True)
+
+
+class Meter(SQLModel, table=False):
+    id: int
+    intervals: list[Interval]
+
+
+# This is one of the objects we get as a response from the endpoint
+# https://docs.bayou.energy/docs/interval-data#interval-data-explained
+# Notice that the sub-element 'meters' contains instances of Meter as defined above
+class IntervalsResponse(SQLModel, table=False):
+    first_interval_discovered: datetime
+    last_interval_discovered: datetime
+    granularities: list[int]
+    meters: list[Meter]
