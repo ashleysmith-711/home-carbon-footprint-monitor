11e1a0628f511998db3a988b05daf99acf098f99
Stephen Suffian <stephen.suffian@pm.me>
2023-12-14 18:13:51 -0500

    Have db auto create on startup

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from sqlmodel import Field, Session, SQLModel, create_engine, select, DateTime, Column
-from .models import CarbonData
+from .models import CarbonData, EnergyData
 from .db import engine
+from .seed import load_sample_energy_data
+from .seed import load_carbon_data
 
 app = FastAPI()
 
@@ -9,6 +11,9 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
+    load_sample_energy_data(customer_id="123", utility="PGE")
+    load_carbon_data(balancing_authority="CISO")
 
 
 @app.get("/api/python")
